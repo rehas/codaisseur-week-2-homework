e300cc14c9825f58866ad9645d6ed2bf601e8b09
RehaS <beratson@gmail.com>
2018-07-13 23:43:18 +0200

    ShoppingCart code cleaning

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,11 +1,12 @@
 class ShoppingCart{
     constructor(){
-        
         this.items = []
     }
+    
     getItems() {
         return this.items
     }
+    
     addItem(itemName, quantity, price){
         const newItem = {
             name : itemName,
@@ -14,22 +15,19 @@ class ShoppingCart{
         }
         this.items.push(newItem)
     }
+    
     clear(){
         this.items = []
     }
+    
     clone(){
         const newCart = new ShoppingCart()
         this.items.map(x=>{
-            
-            // console.log(x)
             newCart.addItem(x.name, x.quantity, x.pricePerUnit)
             return x
         })
-        // console.log(newCart.items)
-        // console.log(this.items)
-
-        // const newCart = JSON.parse(JSON.stringify(this));
         return newCart
     }
 }
+
 module.exports =ShoppingCart
