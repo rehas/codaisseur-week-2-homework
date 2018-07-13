8fb495c049daa56df7681ddf58ad5dcf55c3f6b9
RehaS <beratson@gmail.com>
2018-07-13 23:28:58 +0200

    returning with a delayed promise using giveItBackLater asncy test 3 passed

--- src/async-functions.js
+++ src/async-functions.js
@@ -12,7 +12,13 @@ function giveItBackLater(value, callback){
     setTimeout(loadMe(value), 2000)
     // setTimeout(callback, 2000, value)
 };
+function promiseToGiveItBackLater(value){
+    return new Promise((resolve, reject)=>{
+        giveItBackLater(value, ()=>{
+            return resolve(value)
+        })
+    })
+}
 function addSomePromises(){}
-function promiseToGiveItBackLater(){}
 
 // giveItBackLater("hello", x=>x+x)
