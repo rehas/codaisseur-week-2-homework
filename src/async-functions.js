2c6cb85b644844f7310713d840b7af7e6d56284a
RehaS <beratson@gmail.com>
2018-07-13 23:23:27 +0200

    async test 1-2 passed, callback with setTimeout implemented

--- src/async-functions.js
+++ src/async-functions.js
@@ -4,6 +4,15 @@ module.exports ={
     promiseToGiveItBackLater
 }
 
-function giveItBackLater(){};
+function giveItBackLater(value, callback){
+    function loadMe(x){
+        callback(x)
+        // return x
+    }
+    setTimeout(loadMe(value), 2000)
+    // setTimeout(callback, 2000, value)
+};
 function addSomePromises(){}
 function promiseToGiveItBackLater(){}
+
+// giveItBackLater("hello", x=>x+x)
